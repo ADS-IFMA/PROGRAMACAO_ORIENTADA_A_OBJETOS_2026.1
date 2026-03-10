2d5a7ddb8452e60995a94f819a78e88c96819e96
Bruno Vicente <brunnovicente@gmail.com>
2026-03-10 10:37:26 -0300

    Aula 09032026
    Aula sobre abstração, conceitos básico de classe e objeto

---
+++ AULA1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public void main(){
+
+        Caneta bic = new Caneta();
+        Caneta compacto = new Caneta();
+
+        bic.cor = "Vermelha";
+        compacto.cor = "Azul";
+
+        bic.tamanho = 10;
+
+    }
+
+}
